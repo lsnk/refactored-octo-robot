9869d11bdb724cf036e061a12504d8b89100c9a8
nvkrokosh <nikita.krokosh@fogstream.ru>
2019-07-08 16:36:53 +1000

    Fixed Nginx api urls proxying

--- runme.sh
+++ runme.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-echo APP SHOULD BE AVAILABLE AT: http://0.0.0.0:8787 in a few seconds! && sleep 3 && docker-compose up
+echo APP SHOULD BE AVAILABLE AT: http://0.0.0.0:8787 \(API AT: http://0.0.0.0:8787/api/v1\) in a few seconds! && sleep 3 && docker-compose up

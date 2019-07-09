b63842aa125002dcbfadea1316b76f4e82f4fff8
nvkrokosh <nikita.krokosh@fogstream.ru>
2019-07-09 16:17:26 +1000

    React spa cleanup + docker building

--- spa/src/App.js
+++ spa/src/App.js
@@ -8,21 +8,16 @@ class App extends Component {
     return (
       <div className="container">
         <div className="row">
-
           <div className="col-lg-1"/>
-
           <div className="col-lg-4">
             <center><h2>Users</h2></center>
             <Users/>
           </div>
-
           <div className="col-lg-2"/>
-
           <div className="col-lg-4">
             <center><h2>Groups</h2></center>
             <Groups/>
           </div>
-
           <div className="col-lg-1"/>
         </div>
       </div>
